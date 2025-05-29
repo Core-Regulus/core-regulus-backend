1509bb311438b32f150d73ba08539023a9f8576f
Vladimir Aseev <nemesisv@mail.ru>
2025-05-29 22:51:59 +0200

    change deployment process to docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.24-alpine AS builder
+
+WORKDIR /app
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -ldflags="-s -w" -o ./dist/core-regulus
+
+# Финальный образ
+FROM alpine:latest
+
+WORKDIR /root/
+COPY --from=builder /app/app .
+
+EXPOSE 5000
+CMD ["sh", "-c", "source .env && ./app"]
--- main.go
+++ main.go
@@ -1,92 +1,34 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+    "context"
+    "log"
+    "net"
+    "google.golang.org/grpc"
+		"google.golang.org/grpc/reflection"
+    pb "core-regulus/service.pb" // путь к сгенерированному пакету
 )
 
-type User struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+type server struct {
+    pb.UnimplementedGreeterServer
 }
 
-var users = []User{
-	{ID: 1, Name: "Alice", Email: "alice@example.com"},
-	{ID: 2, Name: "Bob", Email: "bob@example.com"},
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+    return &pb.HelloReply{Message: "Hello, " + in.GetName()}, nil
 }
 
 func main() {
-	app := fiber.New()
+    lis, err := net.Listen("tcp", ":5000")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
 
-	// Получить всех пользователей
-	app.Get("/users", func(c *fiber.Ctx) error {
-		return c.JSON(users)
-	})
+    s := grpc.NewServer()
+    pb.RegisterGreeterServer(s, &server{})
+		reflection.Register(s) 
 
-	// Получить одного пользователя по ID
-	app.Get("/users/:id", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("id")
-		if err != nil {
-			return c.Status(400).SendString("Invalid ID")
-		}
-		for _, u := range users {
-			if u.ID == id {
-				return c.JSON(u)
-			}
-		}
-		return c.Status(404).SendString("User not found")
-	})
-
-	// Создать пользователя
-	app.Post("/users", func(c *fiber.Ctx) error {
-		var user User
-		if err := c.BodyParser(&user); err != nil {
-			return c.Status(400).SendString("Bad Request")
-		}
-		user.ID = len(users) + 1
-		users = append(users, user)
-		return c.Status(201).JSON(user)
-	})
-
-	// Обновить пользователя
-	app.Put("/users/:id", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("id")
-		if err != nil {
-			return c.Status(400).SendString("Invalid ID")
-		}
-
-		var update User
-		if err := c.BodyParser(&update); err != nil {
-			return c.Status(400).SendString("Bad Request")
-		}
-
-		for i, u := range users {
-			if u.ID == id {
-				users[i].Name = update.Name
-				users[i].Email = update.Email
-				return c.JSON(users[i])
-			}
-		}
-
-		return c.Status(404).SendString("User not found")
-	})
-
-	// Удалить пользователя
-	app.Delete("/users/:id", func(c *fiber.Ctx) error {
-		id, err := c.ParamsInt("id")
-		if err != nil {
-			return c.Status(400).SendString("Invalid ID")
-		}
-
-		for i, u := range users {
-			if u.ID == id {
-				users = append(users[:i], users[i+1:]...)
-				return c.SendStatus(204)
-			}
-		}
-
-		return c.Status(404).SendString("User not found")
-	})
-
-	app.Listen(":9001")
+    log.Println("gRPC server listening on :5000")
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
 }
